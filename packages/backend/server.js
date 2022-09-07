714b964966245725e95edf709cbaa9d20ba542bb
baobeta <lequocbao29072001@gmail.com>
2022-09-07 12:45:36 +0700

    add file env

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,9 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const path = require('path');
+// Mapping the schemas in MongoDB into mongoose mode
+require('./api/models');
+
+// eslint-disable-next-line import/no-dynamic-require
+const { mongoUrl } = require(path.join(__dirname, 'mongo_config.js'));
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
 app.use(bodyParser.json());
 
+// setup mongo
+// mongoose.Promise = global.Promise;
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected MongoDB');
+  })
+  .catch((err) => console.log.error(err));
 // allow cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
