47f929d5878ea52056978a4246c17f1ba5be2ea2
baobeta <lequocbao29072001@gmail.com>
2022-11-01 21:07:32 +0700

    add

--- packages/frontend/src/components/Home/BoardTopic.vue
+++ packages/frontend/src/components/Home/BoardTopic.vue
@@ -2,6 +2,7 @@
 <template>
   <div class="flex flex-col h-[800px] shadow-2xl rounded bg-white  mx-10">
     <span class="font-sans font-semibold text-lg mt-4 ml-4"> {{ nameMajor }}</span>
+    <div class="h-full bg-black" />
   </div>
 </template>
 
