1667a2d185737cbec5d7680ffdfacf52ba2ad464
baobeta <lequocbao29072001@gmail.com>
2022-11-05 13:14:47 +0700

    add images

--- packages/frontend/src/components/Home/BoardTopic.vue
+++ packages/frontend/src/components/Home/BoardTopic.vue
@@ -1,12 +1,23 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div class="flex flex-col h-[800px] shadow-2xl rounded bg-white  mx-10">
+  <div class="flex flex-col h-[800px] shadow-2xl rounded bg-white  mx-10 my-10 ">
     <span class="font-sans font-semibold text-lg mt-4 ml-4"> {{ nameMajor }}</span>
-    <div class="h-full bg-black" />
+    <div class="h-full grid gap-x-4 gap-y-4 grid-cols-5 m-4">
+      <img class="h-[300px] bg-gray-600 col-span-2 rounded-lg">
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+      <div class="h-[300px] bg-gray-600 rounded-lg" />
+    </div>
   </div>
 </template>
-
+s
 <script>
+// import imgCNPM from "./"
 
 export default {
   name: 'BoardTopic',
