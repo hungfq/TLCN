a78ecc727e949be57288a026d1bc469911b6c924
baolequoc <bao.lq@holistics.io>
2022-11-06 20:40:10 +0700

    update homepage

--- packages/frontend/src/page/HomePage.vue
+++ packages/frontend/src/page/HomePage.vue
@@ -9,6 +9,17 @@
     <BoardTopic
       class="col-span-4"
       :name-major="'Chuyên ngành công nghệ phần mềm'"
+      :url-image="imageUrlCNPM"
+    />
+    <BoardTopic
+      class="col-span-4"
+      :name-major="'Chuyên ngành hệ thống thông tin'"
+      :url-image="imageUrlHTTT"
+    />
+    <BoardTopic
+      class="col-span-4"
+      :name-major="'Chuyên ngành mạng và an ninh mạng'"
+      :url-image="imageUrlATTT"
     />
   </div>
 </template>
@@ -20,6 +31,10 @@ import TabBar from '../components/Home/TabBar.vue';
 import BannerFrame from '../components/Home/BannerFrame.vue';
 import BannerInfo from '../components/Home/BannerInfo.vue';
 import BoardTopic from '../components/Home/BoardTopic.vue';
+
+const imageUrlCNPM = new URL('../assets/images/Khoa_CNPM.png', import.meta.url).href;
+const imageUrlHTTT = new URL('../assets/images/Khoa_HTTT.jpg', import.meta.url).href;
+const imageUrlATTT = new URL('../assets/images/Khoa_ATTT.png', import.meta.url).href;
 
 export default {
   name: 'HomePage',
@@ -34,6 +49,9 @@ export default {
   },
   data () {
     return {
+      imageUrlCNPM,
+      imageUrlHTTT,
+      imageUrlATTT,
     };
   },
   computed: {
