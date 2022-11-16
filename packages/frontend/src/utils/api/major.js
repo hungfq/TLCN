4bda09a409e18373afc03ee6695205a8978e23b5
Hung Pham <t.hungpham2314@gmail.com>
2022-11-16 21:17:58 +0700

    fix

--- packages/frontend/src/utils/api/major.js
+++ packages/frontend/src/utils/api/major.js
@@ -4,10 +4,15 @@ const apiDest = 'http://localhost:5000/v1';
 axios.defaults.baseURL = apiDest;
 
 export default class MajorApi {
-  static async listAllMajor(token) {
+  static async listAllMajor (token) {
     const res = await axios.get('/majors', {
       headers: {
         authorization: `bearer ${token}`,
+      },
+    });
+    return res.data;
+  }
+
   static async getMajor (token) {
     const res = await axios.get('/majors', {
       headers: {
