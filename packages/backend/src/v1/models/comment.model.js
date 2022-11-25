8cef1b468f078bde39d7d1a103abaa1cfb20da5b
Hung Pham <t.hungpham2314@gmail.com>
2022-11-25 11:12:46 +0700

    fix chính tả

--- packages/backend/src/v1/models/comment.model.js
+++ packages/backend/src/v1/models/comment.model.js
@@ -7,8 +7,8 @@ const CommentSchema = new mongoose.Schema({
   createdBy: { type: Schema.Types.ObjectId },
   timestamps: {
     createdAt: 'created_at', // Use `created_at` to store the created date
-    updatedAt: 'updated_at' // and `updated_at` to store the last updated date
-  }
+    updatedAt: 'updated_at', // and `updated_at` to store the last updated date
+  },
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
