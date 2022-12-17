18257ebe7a65222c2ef6b7342f33461c12bd91d4
baolequoc <bao.lq@holistics.io>
2022-12-17 15:38:43 +0700

    add new field for topic

--- packages/backend/src/v1/models/topic.model.js
+++ packages/backend/src/v1/models/topic.model.js
@@ -7,6 +7,8 @@ const TopicSchema = new mongoose.Schema({
   title: { type: String },
   description: { type: String },
   limit: { type: Number },
+  startDate: { type: Date, default: new Date() },
+  thesis_defense_date: { type: Date },
   deadline: { type: Date },
   major: { type: String },
   lecturerId: {
@@ -14,6 +16,16 @@ const TopicSchema = new mongoose.Schema({
     ref: 'Lecturer',
   },
   students: { type: Array },
+  tasks: { type: Array },
+  calendars: { type: Array },
+  committeePresident: { type: Schema.Types.ObjectId },
+  committeeSecretary: { type: Schema.Types.ObjectId },
+  criticalLecturer: { type: Schema.Types.ObjectId },
+  advisorLecturerGrade: { type: Array },
+  committeePresidentGrade: { type: String },
+  committeeSecretaryGrade: { type: String },
+  criticalLecturerGrade: { type: String },
+
 });
 
 module.exports = mongoose.model('Topic', TopicSchema);
